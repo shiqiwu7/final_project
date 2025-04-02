89a50c93c0df5df79dc4d6e6be424d482dd51264
Shiqi Wu <wushiqi_dia@outlook.com>
2025-04-01 21:56:03 -0400

    change database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_04_01_190424) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.text "description"
