3640379dcce56195916aa79443cdcc48ff23c28f
Shiqi Wu <wushiqi_dia@outlook.com>
2025-04-08 18:04:51 -0400

    fix session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,28 @@
+# class SessionsController < ApplicationController
+#   skip_before_action :authenticate_user!, only: [ :new, :create ]
+
+#   def new
+#   end
+
+#   def create
+#     user = User.find_by(email: params[:email])
+
+#     if user && user.authenticate(params[:password])
+#       session[:user_id] = user.id
+#       redirect_to root_path, notice: "Logged in successfully"
+#     else
+#       flash.now[:alert] = "Invalid email or password"
+#       render :new, status: :unprocessable_entity
+#     end
+#   end
+
+#   def destroy
+#     session[:user_id] = nil
+#     redirect_to root_path, notice: "Logged out successfully"
+#   end
+# end
 class SessionsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :new, :create ]
+  skip_before_action :authenticate_user!, only: [:new, :create]
 
   def new
   end
@@ -7,8 +30,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
 
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    if user && user.valid_password?(params[:password])
+      sign_in user
       redirect_to root_path, notice: "Logged in successfully"
     else
       flash.now[:alert] = "Invalid email or password"
@@ -17,7 +40,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    sign_out current_user
     redirect_to root_path, notice: "Logged out successfully"
   end
 end
