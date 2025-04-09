83f0016ada2e956eae5cdc03680fc58e46ae11fa
Shiqi Wu <wushiqi_dia@outlook.com>
2025-04-08 21:39:28 -0400

    change encry passwd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_08_223154) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_013733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -126,7 +126,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_08_223154) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
