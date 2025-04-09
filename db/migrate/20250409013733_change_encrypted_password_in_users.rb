b2c19cecdabdf3596ccd59798734c4854c28bc05
Shiqi Wu <wushiqi_dia@outlook.com>
2025-04-08 23:12:01 -0400

    fix encry

--- db/migrate/20250409013733_change_encrypted_password_in_users.rb
+++ db/migrate/20250409013733_change_encrypted_password_in_users.rb
@@ -1,5 +1,5 @@
 class ChangeEncryptedPasswordInUsers < ActiveRecord::Migration[8.0]
   def change
-    change_column :users, :encrypted_password, :string, null: true
+    # change_column :users, :encrypted_password, :string, null: true
   end
 end
