43e980b2e3647bbff552c9c75e477dc9be946eb7
Shiqi Wu <wushiqi_dia@outlook.com>
2025-04-08 23:30:29 -0400

    change migrate

--- db/migrate/20250401184711_drop_team_memberships.rb
+++
@@ -1,5 +0,0 @@
-class DropTeamMemberships < ActiveRecord::Migration[8.0]
-  def change
-    drop_table :team_memberships
-  end
-end
--- db/migrate/20250409013733_change_encrypted_password_in_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeEncryptedPasswordInUsers < ActiveRecord::Migration[8.0]
-  def change
-    # change_column :users, :encrypted_password, :string, null: true
-  end
-end
