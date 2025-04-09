4417ad7ff906c94df72780cdc41f0ce7450e941a
Shiqi Wu <wushiqi_dia@outlook.com>
2025-04-08 22:30:18 -0400

    fix devise.rb

--- db/migrate/20250407184859_add_devise_to_users.rb
+++ db/migrate/20250407184859_add_devise_to_users.rb
@@ -3,10 +3,10 @@
 class AddDeviseToUsers < ActiveRecord::Migration[8.0]
   def self.up
     change_table :users do |t|
-      # t.string :email,              null: false, default: ""
-      # t.string :encrypted_password, null: false, default: ""
-      # t.string   :reset_password_token
-      # t.datetime :reset_password_sent_at
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at unless column_exists?(:users, :remember_created_at)
@@ -30,8 +30,8 @@ class AddDeviseToUsers < ActiveRecord::Migration[8.0]
       # t.timestamps null: false  # 你已经有 created_at 和 updated_at，无需再加
     end
 
-    # add_index :users, :email, unique: true
-    # add_index :users, :reset_password_token, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token, unique: true
     # add_index :users, :unlock_token, unique: true
   end
